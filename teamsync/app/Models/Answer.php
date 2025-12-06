42408a76931a26451460826f740c26bd47826834
MoriSuzu <suzu.0522.suzu@icloud.com>
2025-12-07 04:25:17 +0900

    モデルを作成しました

--- teamsync/app/Models/Answer.php
+++ teamsync/app/Models/Answer.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Answer extends Model
 {
-    //
+    protected $fillable = ['user_id', 'question_id', 'score', 'comment', 'week_number'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
